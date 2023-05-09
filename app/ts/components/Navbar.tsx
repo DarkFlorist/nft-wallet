90faff529030bd4ec4c1f34e93e682e89ded879f
0xjimmy <me@0xjimmy.xyz>
2023-05-10 11:56:10 +1200

    Make input styling consistent, show blockies on NFT details, link to metadata

--- app/ts/components/Navbar.tsx
+++ app/ts/components/Navbar.tsx
@@ -13,12 +13,12 @@ export const Navbar = ({
 }) => {
 	return (
 		<div className='flex flex-col w-full sm:flex-row items-center justify-between gap-4 border-slate-400/30'>
-			<h1 className='font-extrabold text-4xl'>NFT Wallet</h1>
+			<h1 className='text-xl'>NFT Sender</h1>
 			<div className='flex gap-4 items-center'>
 				{provider.value ? (
 					<>
-						<div className='flex flex-col items-end'>
-							<p className='font-bold'>{provider.value.walletAddress}</p>
+						<div className='flex flex-col'>
+							<p className='font-bold break-all'>{provider.value.walletAddress}</p>
 							<span className='text-gray-400 text-sm'>{provider.value.chainId === 1n ? 'Mainnet' : `Network ${provider.value.chainId.toString()}`}</span>
 						</div >
 						<Blockie seed={provider.value.walletAddress.toLowerCase()} size={12} />
