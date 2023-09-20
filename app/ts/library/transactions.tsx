8f3916edbf9e74d970454083bd36055b95df5fba
0xjimmy <me@0xjimmy.xyz>
2023-09-20 16:36:18 +1200

    Add Notice component and display error and sucess of transaction requests

--- app/ts/library/transactions.tsx
+++ app/ts/library/transactions.tsx
@@ -1,15 +1,14 @@
+import { TransactionResponse } from 'ethers';
 import { Contract, BrowserProvider } from 'ethers'
 import { ERC1155ABI, ERC721ABI } from './abi.js'
 import { ERC1155, ERC721 } from './identifyTokens.js'
 
-export async function transferERC721(nft: ERC721, recipient: string, provider: BrowserProvider) {
+export async function transferERC721(nft: ERC721, recipient: string, provider: BrowserProvider): Promise<TransactionResponse> {
 	const contract = new Contract(nft.address, ERC721ABI, await provider.getSigner())
-	const tx = await contract.transferFrom(nft.owner, recipient, nft.id);
-	await tx.wait()
+	return await contract.transferFrom(nft.owner, recipient, nft.id);
 }
 
-export async function transferERC1155(nft: ERC1155, from: string, recipient: string, amount: bigint, provider: BrowserProvider) {
+export async function transferERC1155(nft: ERC1155, from: string, recipient: string, amount: bigint, provider: BrowserProvider): Promise<TransactionResponse> {
 	const contract = new Contract(nft.address, ERC1155ABI, await provider.getSigner())
-	const tx = await contract.safeTransferFrom(from, recipient, nft.id, amount, "0x");
-	await tx.wait()
+	return await contract.safeTransferFrom(from, recipient, nft.id, amount, "0x");
 }
